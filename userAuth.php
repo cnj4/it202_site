c5ecc6e14bf64dd673626029fe57995eeb4ebfd5
CC <cnj4@njit.edu>
2016-03-12 22:23:58 -0500

    obscured password field & started post-login behavior

--- userAuth.php
+++ userAuth.php
@@ -12,22 +12,39 @@ case "register":
     $db = new userDB("connect.ini");
     $db->addNewUser($name,$password);
     $result = "New user $name created!";
+    $link = "return";
     break;
-case 'login':
+
+case "login":
     $name = $_POST['username'];
     $password = $_POST['password'];
     $db = new userDB("connect.ini");
     if ($db->validateUser($name,$password))
     {
         $result = "Login Successful!";
+        $link = "continue";
     }
     else
     {
         $result = "Invalid Login!";
+        $link = "return";
     }
     break;
 }
 
 echo $result.PHP_EOL;
 
+switch($link)
+{
+case "return":
+    //TODO:HTML return to login link
+    echo "<a href=\"index.html\">return to login</a>".PHP_EOL;
+    echo "Return Link goes here".PHP_EOL;
+    break;
+
+case "contirnue":
+    //TODO:HTML continue to chat link
+    echo "Return Link goes here".PHP_EOL;
+    break;
+}
 ?>
